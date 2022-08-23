1cfa546ca6868eb42a22d036e9a8b6ad2a1b6739
韩旭 <js_bin@163.com>
2022-08-23 19:56:55 +0800

    feature: top-up util funtions add

--- test/unit/top-up.test.js
+++ test/unit/top-up.test.js
@@ -6,10 +6,21 @@ const {
     topUpTusdByAddress,
     topUpUsdpByAddress,
     topUpBusdByAddress,
+    topUpGusdByAddress,
+    topUpSusdByAddress,
+    topUpWETHByAddress,
+    topUpSTETHByAddress,
+    topUpWstEthByAddress,
+    topUpRocketPoolEthByAddress,
+    topUpSEthByAddress,
+    topUpSEth2ByAddress,
+    topUpREth2ByAddress,
+    topUpEthByAddress
 } = require("../../utils/top-up-utils")
 const Utils = require("../../utils/assert-utils")
 const addresses = require("../../config/address-config")
 const ERC20 = hre.artifacts.require("@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20")
+const { balance } = require("@openzeppelin/test-helpers")
 
 let accounts
 let farmer1
@@ -75,5 +86,90 @@ describe("【Verify】Recharge Script", function () {
         Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
         await topUpBusdByAddress(topUpAmount, farmer1)
         Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpGusdByAddress", async function () {
+        const contract = await ERC20.at(addresses.GUSD_ADDRESS)
+        const topUpAmount = 1e9 * 1e2
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpGusdByAddress(topUpAmount, farmer1)
+        // TODO: build more
+        // Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpSusdByAddress", async function () {
+        const contract = await ERC20.at(addresses.SUSD_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpSusdByAddress(topUpAmount, farmer1)
+        // TODO: build more
+        // Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpREth2ByAddress", async function () {
+        const contract = await ERC20.at(addresses.rETH2_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpREth2ByAddress(topUpAmount, farmer1)
+        // TODO: build more
+        // Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpSEth2ByAddress", async function () {
+        const contract = await ERC20.at(addresses.sETH2_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpSEth2ByAddress(topUpAmount, farmer1)
+        // TODO: build more
+        // Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpSEthByAddress", async function () {
+        const contract = await ERC20.at(addresses.sETH_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpSEthByAddress(topUpAmount, farmer1)
+        // TODO: build more
+        // Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpRocketPoolEthByAddress", async function () {
+        const contract = await ERC20.at(addresses.rocketPoolETH_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpRocketPoolEthByAddress(topUpAmount, farmer1)
+        Utils.assertBNGte(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpWstEthByAddress", async function () {
+        const contract = await ERC20.at(addresses.wstETH_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpWstEthByAddress(topUpAmount, farmer1)
+        Utils.assertBNGte(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpSTETHByAddress", async function () {
+        const contract = await ERC20.at(addresses.stETH_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpSTETHByAddress(topUpAmount, farmer1)
+        Utils.assertBNGte(await contract.balanceOf(farmer1), topUpAmount)
+    })
+    
+    it("topUpWETHByAddress", async function () {
+        const contract = await ERC20.at(addresses.WETH_ADDRESS)
+        const topUpAmount = 1e9 * 1e18
+        Utils.assertBNEq(await contract.balanceOf(farmer1), 0)
+        await topUpWETHByAddress(topUpAmount, farmer1)
+        Utils.assertBNEq(await contract.balanceOf(farmer1), topUpAmount)
+    })
+
+    it("topUpEthByAddress", async function () {
+        const balanceBefore = await balance.current(farmer1)
+        const topUpAmount =  1e9 * 1e18
+        await topUpEthByAddress(topUpAmount, farmer1)
+        const balanceAfter = await balance.current(farmer1)
+        Utils.assertBNEq(balanceAfter.sub(balanceBefore), topUpAmount)
     })
 })
